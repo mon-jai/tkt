908e792e93fc4082c206c719cc7fc5db15a4f461
Kivxx <154081045+CharonTung@users.noreply.github.com>
2025-05-23 15:16:30 +0800

    新增semster_json與score_json

---
+++ lib/models/semester/semester_json.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'semester_json.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SemesterJson _$SemesterJsonFromJson(Map<String, dynamic> json) => SemesterJson(
+      year: json['year'] as String,
+      semester: json['semester'] as String,
+    );
+
+Map<String, dynamic> _$SemesterJsonToJson(SemesterJson instance) =>
+    <String, dynamic>{
+      'year': instance.year,
+      'semester': instance.semester,
+    };
