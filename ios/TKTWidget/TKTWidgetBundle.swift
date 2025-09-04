d6ce13fb1f7d7b5ac32c4a2447fd8274075b4f44
Kiv <154081045+kivxxx@users.noreply.github.com>
2025-09-04 14:13:50 +0800

    小工具模板

---
+++ ios/TKTWidget/TKTWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TKTWidgetBundle.swift
+//  TKTWidget
+//
+//  Created by Kiv on 2025/9/4.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TKTWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TKTWidget()
+    }
+}
