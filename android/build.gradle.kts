33bf2fb05d660e8cb6f5046f7535d327274a36d7
Mon <91261297+mon-jai@users.noreply.github.com>
2025-09-10 19:46:00 +0800

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,22 +5,22 @@ allprojects {
     }
 }
 
+val packageNamespace = "com.monjai.tkt"
+project.extensions.extraProperties["namespace"] = packageNamespace
+
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+    // You can also propagate the namespace to subprojects if needed
+    project.extensions.extraProperties["namespace"] = packageNamespace
 }
-
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
-}
-
-android {
-    namespace = "com.example.yourapp"
 }
